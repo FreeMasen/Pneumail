b440976ddc71670841068da234d317715cc12313
Robert Masen <rmasen@USIHQ.net>
2017-09-13 10:54:32 -0500

    initial

---
+++ ts/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import AppContainer from './appContainer';
+
+window.onload = () => {
+    ReactDOM.render(
+        <AppContainer />,
+        document.querySelector('main')
+    )
+}
---
+++ ts/appContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class AppContainer extends React.Component<any, any> {
+    render() {
+        return (
+            <div id="app-container">
+            
+            </div>
+        )
+    }
+}
